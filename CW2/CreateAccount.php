f8f63d5d49119b68ee502ff595e454ead7614163
Tommy Lamb <james003pond@gmail.com>
2016-11-06 15:45:49 +0000

    Started CW2
    Created the first version of the DB, and a preliminary CreateNewUser PHP
    script

---
+++ CW2/CreateAccount.php
@@ -0,0 +1,44 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+$dsn = 'mysql:dbname=til1;host=mysql-server-1.macs.hw.ac.uk;charset=utf8';
+$db = new PDO($dsn, 'til1', 'abctil1354');
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$required = ['Username','Password','FName','SName','TNumber','Line1','Line2','Line3','Line4','City','Postcode','County','Country'];
+
+foreach ($required as $field){
+	if (!strlen($_POST[$field])) {
+		#Great big dirty error
+	} else {
+		$values[] = $field;
+	}
+}
+
+$values = array_reverse($values);
+
+for ($i = 0; $i<=4 ; $i++){
+	$customerValues[] = array_pop($values);
+}
+
+for ($i = 0; $i<=8; $i++){
+	$addressValues[] = array_pop($values);
+}
+
+$customerStatement = $db->prepare('insert into Customer (Username,Password,FName,SName, TNumber) VALUES (:Username,:Password,:FName,:SName,:TNumber)');
+$addressStatement = $db->prepare('insert into Address (UID,Line1,Line2,Line3,Line4,City,Postcode,County,Country) Values (:UID,:Line1,:Line2,:Line3,:Line4,:City,:Postcode,:County,:Country)');
+
+if ($customerStatement->execute($customerValues)){
+	echo 'CustomerSuccess';
+} else {
+	echo 'CustomerFail';
+}
+
+if ($addressStatement->execute($addressValues)){
+	echo 'AddressSuccess';
+} else {
+	echo 'AddressFailure';
+}
+?>
---
+++ CW2/CreateDatabase.sql
@@ -0,0 +1,56 @@
+drop table Customer;
+drop table Address;
+drop table Transaction;
+drop table TransactionProductList;
+drop table Favourites;
+
+create table Customer(
+UID int primary key AUTO_INCREMENT,
+Username varchar(128) UNIQUE not null,
+Password varchar(255),
+FName varchar(64) not null,
+SName varchar(64) not null,
+TNumber varchar(24)
+)ENGINE=INNODB;
+
+create table Address(
+AddressID int primary key AUTO_INCREMENT,
+UID varchar(128) not null,
+Line1 varchar(64) not null,
+Line2 varchar(64),
+Line3 varchar(64),
+Line4 varchar(64),
+City varchar(64) not null,
+Postcode varchar(24) not null,
+County varchar(64) not null,
+Country varchar(64) not null,
+foreign key (UID) references Customer(UID)
+)ENGINE=INNODB;
+
+create table Transaction(
+TransactionTimestamp DATETIME(6) primary key,
+UID varchar(128),
+AddressID int not null,
+DeliveryStatus int not null,
+NoProducts int,
+Cost DECIMAL(6,2),
+foreign key (UID) references Customer(UID),
+foreign key (AddressID) references Address(AddressID)
+)ENGINE=INNODB;
+
+create table TransactionProductList(
+TransactionTimestamp DATETIME(6) not null,
+ProductName varchar(64) not null,
+Amount int(3) not null,
+foreign key (TransactionTimestamp) references Transaction(TransactionTimestamp),
+foreign key (ProductName) references ProductList(ProductName),
+primary key (TransactionTimestamp, ProductName)
+)ENGINE=INNODB;
+
+create table Favourites(
+ProductName varchar(64) not null,
+UID varchar(128) not null,
+foreign key (ProductName) references ProductList(ProductName),
+foreign key (UID) references Customer(UID),
+primary key (ProductName, UID)
+)ENGINE=INNODB;
